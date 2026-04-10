08f66c5178ed3eee7e496cb51ea2c182dc44b643
Pranav Mandala <pranav.mandala07@gmail.com>
2026-04-10 12:49:24 -0700

    Updating Files

--- java/CIS-35A/TemperatureConverter.java
+++ java/CIS-35A/TemperatureConverter.java
@@ -15,7 +15,38 @@ public class TemperatureConverter {
     
     public static void main(String[] args) {
         
+        //Declaring variables to hold given and calculated temperatures
+        double celsius;
+        double fahrenheit;
+        double calculatedCelsius;
+        double calculatedFahrenheit;
+        
+        //Creating a scanner object
         Scanner s = new Scanner(System.in);
+        
+        System.out.printf("=== Temperature Converter ===\n\n");
+        
+        //Prompting user for celsius input and initializing
+        System.out.printf("Enter a temperature in Celsius: ");
+        celsius = s.nextDouble();
+        
+        //Converting celsius to fahrenheit
+        calculatedFahrenheit = 32 + celsius * (180.00 / 100.00);
+        
+        //Outputting celsius and converted fahrenheit
+        System.out.printf("%.2f\u00B0C = %.2f\u00B0F\n\n", celsius, calculatedFahrenheit);
+        
+        //Prompting user for fahrenheit input and intializing
+        System.out.printf("Enter a temperature in Fahrenheit: ");
+        fahrenheit = s.nextDouble();
+        
+        //Converting fahrenheit to celsius
+        calculatedCelsius = (fahrenheit - 32) * (100.00 / 180.00);
+        
+        //Outputting fahrenheit and converted celsius
+        System.out.printf("%.2f\u00B0F = %.2f\u00B0C", fahrenheit, calculatedCelsius);
+        
+        s.close();
         
     }
 }
